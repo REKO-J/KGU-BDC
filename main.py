51dc123b49c0ed197d4f9ccf6ec6d3c783432230
YeongHun Jo <117628869+REKO-J@users.noreply.github.com>
2023-11-13 17:13:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -133,3 +133,5 @@ if st.session_state['generated']:
     for i in range(len(st.session_state['generated'])-1, -1, -1):
         message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
         message(st.session_state["generated"][i], key=str(i))
+
+st.session_state.generated.append('안녕하세요! 빅데이터 캠프에 대해 궁금한 점이 있으신가요? 도움이 필요하시면 언제든지 말씀해주세요.')

ac84fd9d5dc97ce7cdfabfd583096ea97bb34b50
YeongHun Jo <117628869+REKO-J@users.noreply.github.com>
2023-11-14 13:24:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -146,29 +146,3 @@ if st.session_state.messages[-1]["role"] != "assistant":
 # st.header("🤖 경기대 빅데이터센터 Chatbot(Demo)")
 # st.markdown("'gpt-3.5-turbo'를 기반으로 만들어진 챗봇입니다.")
 # st.markdown("[홈페이지](https://www.kgu-bigdata.com/default/)")
-
-"""
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = ["안녕하세요! 빅데이터 캠프에 대해 궁금한 점이 있으신가요? 도움이 필요하시면 언제든지 말씀해주세요."]
-
-if 'past' not in st.session_state:
-    st.session_state['past'] = ["안녕하세요"]
-
-# 텍스트를 입력하여 봇과 대화 할 수 있는 폼 생성
-# clear_on_submit 옵션을 통해서 submit 하면 폼의 내용이 지워짐
-with st.form('form', clear_on_submit=True):
-    user_input = st.text_input('나:', '', key='input')
-    submitted = st.form_submit_button('전송')
-
-# 메시지를 입력 후 전송을 누를 경우
-if submitted and user_input:
-    output = ask(user_input)
-    st.session_state.past.append(user_input)
-    st.session_state.generated.append(output)
-
-# 저장된 대화 내용 보여주기
-if st.session_state['generated']:
-    for i in range(len(st.session_state['generated'])-1, -1, -1):
-        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-        message(st.session_state["generated"][i], key=str(i))
-"""
